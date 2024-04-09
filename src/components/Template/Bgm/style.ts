d07294f340b9143dafb7190c0d7ba8316a237e00
LeeByoungWook <lbw3973@naver.com>
2024-04-09 16:25:10 +0900

    feat: BGM 컴포넌트 fixed로 변경

--- src/components/Template/Bgm/style.ts
+++ src/components/Template/Bgm/style.ts
@@ -1,12 +1,16 @@
 import styled from "styled-components";
 
 export const AudioWrapper = styled.div`
-  position: absolute;
-  top: 10px;
-  right: 20px;
+  position: fixed;
+  top: 0;
   z-index: 100;
+  width: 100%;
+  max-width: 450px;
+  display: flex;
+  justify-content: end;
 
   .audio-image {
+    padding: 20px;
     cursor: pointer;
   }
 `;
