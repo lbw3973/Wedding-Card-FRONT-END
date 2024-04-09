e08967ce1aab974215cdcaff10df24ddfef30d9c
LeeByoungWook <lbw3973@naver.com>
2024-04-09 20:18:16 +0900

    design: 마이페이지 배경색 변경

--- src/pages/Mypage/style.ts
+++ src/pages/Mypage/style.ts
@@ -3,5 +3,5 @@ import styled from "styled-components";
 export const Section = styled.section`
   padding: 10px;
   position: relative;
-  background-color: #f1f1f1;
+  background-color: #ececec;
 `;
